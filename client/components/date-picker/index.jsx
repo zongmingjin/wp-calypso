d8722f138ebae4537330cc9e3dadac10a017b379
jakejones1984 <40353716+jakejones1984@users.noreply.github.com>
2018-06-30 18:05:50 +0100

    Fixes errors generated by missing check for selectedDays prop
    Prevously code attempted to access `from` and `to` properties of the `selectedDays` Component prop before checking that `selectedDays` existed.

--- client/components/date-picker/index.jsx
+++ client/components/date-picker/index.jsx
@@ -176,6 +176,9 @@ class DatePicker extends PureComponent {
 			sunday: { daysOfWeek: [ 0 ] },
 		};
 
+		const hasDateRange =
+			this.props.selectedDays && this.props.selectedDays.from && this.props.selectedDays.to;
+
 		if ( this.props.selectedDay ) {
 			modifiers[ 'is-selected' ] = this.getDateInstance( this.props.selectedDay );
 		}
@@ -186,7 +189,7 @@ class DatePicker extends PureComponent {
 			);
 		}
 
-		if ( this.props.selectedDays.from && this.props.selectedDays.to ) {
+		if ( hasDateRange ) {
 			modifiers[ 'range-start' ] = this.props.selectedDays.from;
 			modifiers[ 'range-end' ] = this.props.selectedDays.to;
 			modifiers.range = {
@@ -197,7 +200,7 @@ class DatePicker extends PureComponent {
 
 		const rootClassNames = classNames( {
 			'date-picker': true,
-			'date-picker--has-range': this.props.selectedDays.from && this.props.selectedDays.to,
+			'date-picker--has-range': hasDateRange,
 		} );
 
 		return (
