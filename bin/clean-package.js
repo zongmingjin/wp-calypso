f6680fe386bd04820d125b2f76b9a167e2931b74
Ben Lowery <ben.lowery@automattic.com>
2018-11-12 10:49:42 -0500

    formatting

--- bin/clean-package.js
+++ bin/clean-package.js
@@ -1,7 +1,9 @@
+/** @format */
+
 const rimraf = require( 'rimraf' );
-const process = require('process');
+const process = require( 'process' );
 
 console.log( `cleaning ${ process.cwd() }` );
 rimraf( 'dist/**/*', err => {
 	console.error( err );
-}  );
+} );
